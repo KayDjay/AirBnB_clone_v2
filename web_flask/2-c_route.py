2a9386a57336275e982f36ae17dcfb35c5f19452
root <Khadijatrasaq@gmail.com>
2023-12-26 07:10:17 -0800

    Modification

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,12 +1,13 @@
 #!/usr/bin/pytho3
-""" 
+"""
 A script that starts a Flask web application:
 
 Your web application must be listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
-/c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )
+/c/<text>: display “C ” followed by the value of the text
+variable (replace underscore _ symbols with a space )
 You must use the option strict_slashes=False in your route definition
 """
 
@@ -14,6 +15,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
